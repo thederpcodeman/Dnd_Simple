29fe36a62676b2bc1336cfcccd23e9ae8fd33aa6
connertm24 <connermckeone42@gmail.com>
2024-04-24 10:31:26 -0700

    Question

--- src/Question.java
+++ src/Question.java
@@ -1,4 +1,25 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Question {
+    public static boolean rand;
+    public static Option ask(ArrayList<Option> l, String promt){
+        if (rand){
+            return l.get((int) (Math.random() * l.size()));
+        }else{
+            int uin = -1;
+            Scanner sc = new Scanner(System.in);
+            while (!(uin >= 0 && uin < l.size())){
+                System.out.println(promt);
+                System.out.println("Choices:");
+                for (Option i: l){
+                    System.out.println(l.indexOf(i) + ": " + i.name);
+                }
+                uin = sc.nextInt();
+            }
+            return l.get(uin);
+        }
+    }
 }
