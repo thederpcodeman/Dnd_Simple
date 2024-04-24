b752dab9dda4feb96f2d294d69db5470e9a260c9
connertm24 <connermckeone42@gmail.com>
2024-04-24 09:55:41 -0700

    First Post

---
+++ src/Choice.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Choice {
+    public Choice(){
+    }
+    public ArrayList<Option> run(){
+        ArrayList<Option> list = new ArrayList<Option>();
+        return list;
+    }
+}
---
+++ src/Option.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Option {
+    public String name;
+    public Option(String n){
+        name = n;
+    }
+    public ArrayList<Option> run(){
+        ArrayList<Option> list = new ArrayList<Option>();
+        return list;
+    }
+}
