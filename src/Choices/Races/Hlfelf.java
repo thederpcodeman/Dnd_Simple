a8e06bf5af816c7ba28ba107ed371c713e5d15dc
connertm24 <connermckeone42@gmail.com>
2024-04-30 10:31:09 -0700

    Half-Elf Versatility Label improvement

--- src/Choices/Races/Hlfelf.java
+++ src/Choices/Races/Hlfelf.java
@@ -13,13 +13,13 @@ public class Hlfelf extends Choice {
     public ArrayList<Option> run(){
         ArrayList<Option> list = new ArrayList<Option>();
         ArrayList<Option> pick = new ArrayList<Option>();
-        pick.add(new Option("Skill Versatility (Half-Elf)"));
-        pick.add(new Option("Elf Weapon Training (Half-Elf)"));
-        pick.add(new Option("Cantrip (Half-Elf)"));
-        pick.add(new Option("Fleet of Foot (Half-Elf)"));
-        pick.add(new Option("Mask of the Wild (Half-Elf)"));
-        pick.add(new Option("Drow Magic (Half-Elf)"));
-        pick.add(new Option("Swim Speed (Half-Elf)"));
+        pick.add(new Option("Skill Versatility (Half-Elf Versatility)"));
+        pick.add(new Option("Elf Weapon Training (Half-Elf Versatility)"));
+        pick.add(new Option("Cantrip (Half-Elf Versatility)"));
+        pick.add(new Option("Fleet of Foot (Half-Elf Versatility)"));
+        pick.add(new Option("Mask of the Wild (Half-Elf Versatility)"));
+        pick.add(new Option("Drow Magic (Half-Elf Versatility)"));
+        pick.add(new Option("Swim Speed (Half-Elf Versatility)"));
 
         Option a = Question.ask(pick, "Choose your Half-Elf Versatility Feature");
         list.add(a);
