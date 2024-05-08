18501f9550f48f6721f82b9ebc81f7994d295ceb
connertm24 <connermckeone42@gmail.com>
2024-05-08 10:02:24 -0700

    Subclass stuff, Ranger

--- src/Choices/Classes/RanSub.java
+++ src/Choices/Classes/RanSub.java
@@ -15,17 +15,30 @@ public class RanSub extends Choice {
     public ArrayList<Option> run(){
         ArrayList<Option> list = new ArrayList<Option>();
         ArrayList<Option> pick = new ArrayList<Option>();
-        pick.add(new Option("Beast Master (Ranger)"));
         pick.add(new Subchoice("Beast Master (Ranger)", new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList(
                 new Option ("Ranger's Companion (Non-Variant Feature)"),
                 new Option ("Primal Companion (Variant Feature)")))))));
-        pick.add(new Option("Fey Wonderer (Ranger)"));
-        pick.add(new Option("Gloom Stalker (Ranger)"));
-        pick.add(new Option("Horizon Walker (Ranger)"));
-        pick.add(new Option("Hunter (Ranger)"));
+        pick.add(new Option("Fey Wonderer (Ranger)"));//
+        pick.add(new Option("Gloom Stalker (Ranger)"));//
+        pick.add(new Option("Horizon Walker (Ranger)"));//
+        pick.add(new Subchoice("Hunter (Ranger)", new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList(
+                        new Option ("Colossus Slayer (3rd level Feature)"),
+                        new Option ("Giant Killer (3rd level Feature)"),
+                        new Option ("Horde Breaker (3rd level Feature)"))),
+                new ArrayList<>(Arrays.asList(
+                        new Option ("Escape the Horde (7th level Feature)"),
+                        new Option ("Multiattack Defense (7th level Feature)"),
+                        new Option ("Steel Will (7th level Feature)"))),
+                new ArrayList<>(Arrays.asList(
+                        new Option ("Volley (11th level Feature)"),
+                        new Option ("Whirlwind Attack (11th level Feature)"))),
+                new ArrayList<>(Arrays.asList(
+                        new Option ("Evasion (15th level Feature)"),
+                        new Option ("Stand Against the Tide (15th level Feature)"),
+                        new Option ("Uncanny Dodge (15th level Feature)")))))));
         pick.add(new Option("Monster Slayer (Ranger)"));
         pick.add(new Option("Swarmkeeper (Ranger)"));
-        pick.add(new Option("Drakewarden (Ranger)"));
+        pick.add(new Option("Drakewarden (Ranger)"));//
         Option a = Question.ask(pick, "Choose your Ranger Conclave");
         list.add(a);
         pick = new ArrayList<Option>();
